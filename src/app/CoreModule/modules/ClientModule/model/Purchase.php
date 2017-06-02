52aa9ca87c4c04cbda5cc83074483caadcd721c5
Petr Blazicek <petr.blazicek@pb-soft.cz>
2017-06-03 01:06:18 +0200

    Purchase finalized

--- src/app/CoreModule/modules/ClientModule/model/Purchase.php
+++ src/app/CoreModule/modules/ClientModule/model/Purchase.php
@@ -44,4 +44,118 @@ class Purchase extends CreatedEntity
 	 * @var PurchaseItem|ArrayCollection
 	 */
 	private $items;
+
+	// getters & setters
+
+
+	/**
+	 * @return string
+	 */
+	public function getNote()
+	{
+		return $this->note;
+	}
+
+
+	/**
+	 * @param string $note
+	 * @return self (fluent interface)
+	 */
+	public function setNote( $note )
+	{
+		$this->note = $note;
+		return $this;
+	}
+
+
+	/**
+	 * @return Client
+	 */
+	public function getClient()
+	{
+		return $this->client;
+	}
+
+
+	/**
+	 * @param Client $client
+	 * @return self (fluent interface)
+	 */
+	public function setClient( $client )
+	{
+		$this->client = $client;
+		return $this;
+	}
+
+
+	/**
+	 * @return Card
+	 */
+	public function getCard()
+	{
+		return $this->card;
+	}
+
+
+	/**
+	 * @param Card $card
+	 * @return self (fluent interface)
+	 */
+	public function setCard( $card )
+	{
+		$this->card = $card;
+		return $this;
+	}
+
+
+	/**
+	 * @return Shop
+	 */
+	public function getShop()
+	{
+		return $this->shop;
+	}
+
+
+	/**
+	 * @param Shop $shop
+	 * @return self (fluent interface)
+	 */
+	public function setShop( $shop )
+	{
+		$this->shop = $shop;
+		return $this;
+	}
+
+
+	/**
+	 * @return PurchaseItem|ArrayCollection
+	 */
+	public function getItems()
+	{
+		return $this->items;
+	}
+
+
+	/**
+	 * @param mixed $item
+	 * @return self (fluent interface)
+	 */
+	public function addItem( $item )
+	{
+		$this->items->add( $item );
+		$item->setPurchase( $this );
+		return $this;
+	}
+
+
+	/**
+	 * @param mixed $item
+	 * @return self (fluent interface)
+	 */
+	public function removeItem( $item )
+	{
+		$this->items->removeElement( $item );
+		return $this;
+	}
 }
